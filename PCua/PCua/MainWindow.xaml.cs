969bde6270efdf6b9f6e370dd2f59c6a1bb0de5a
LukasMMarinovic <lukas@agramer.com>
2016-02-15 21:27:11 -0600

    PCua v 0.0.0.1
    +text label
    +way to write the CPU name in the label
    +CPU class
    -good programming style

---
+++ PCua/PCua/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Management;
+
+//|=======================|//
+//| PCua                  |//
+//|                       |//
+//| Analyzes current      |//
+//| PC build and offers   |//
+//| upgrade suggestions   |//
+//|=======================|//
+
+//|=================================================|//
+//|Scratch-A-Bear inc.                              |//
+//|                                                 |//
+//|please visit:                                    |//
+//|      http://www.scratch-a-bear.edu              |//     
+//|                                                 |//
+//|Developers:                                      |//
+//|      Lukas Marinovic                            |//
+//|         https://github.com/LukasMMarinovic      |//
+//|      Vijaysrinivas Rajagopal                    |//
+//|         https://github.com/MedicAlert           |//
+//|=================================================|//
+
+
+
+
+namespace PCua
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+           
+            string info="";
+
+            ManagementObjectSearcher CPUdetail = new ManagementObjectSearcher("SELECT name FROM Win32_Processor");
+            ManagementObjectCollection details = CPUdetail.Get();
+            foreach (ManagementObject detail in details)
+            {
+                info = (detail["name"].ToString());
+            }
+
+
+            lbl_TestOutput.Content = info;
+
+            
+        }
+    }
+}
+public partial class CPU
+{
+    
+}
